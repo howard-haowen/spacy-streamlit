d06eaadd6db242067884332cf73e20d3a4cbe446
Haowen Jiang <howard.haowen@gmail.com>
2023-03-07 11:53:34 +0800

    Add link to the creator's landing page

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ st.markdown("""
 
 ## Language Learning Assisted by AI Models
 
-- 開啟左側選單可選擇語言，目前支援華語、日語和英語。
-  - Select a language from the sidebar. Supported languages include Mandarin, Japanese, and English.
+- 開啟左側選單可選擇語言，目前僅支援華語、日語和英語。
+  - Select a language from the sidebar. Currently, only Mandarin, Japanese, and English are supported.
 - 選單自動隱藏時，點選左上角 > 符號以開啟選單。
   - If the sidebar is hidden, click on the > symbol in the upper left corner to open it.
-  
+- [點此]((https://howard-haowen.rohan.tw/))開啟作者個人首頁。
+  - Click [here]((https://howard-haowen.rohan.tw/)) to open the creator's portfolio page.
 """)
